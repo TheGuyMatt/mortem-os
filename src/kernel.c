e996ae6cb0cee5755fed2dba5698878eea92dff6
TheGuyMatt <matthew.hieb@protonmail.com>
2021-06-09 23:17:31 -0700

    added newline support for kernel terminal

--- src/kernel.c
+++ src/kernel.c
@@ -96,14 +96,20 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
   terminal_buffer[index] = vga_entry(c, color);
 }
 
-void terminal_putchar(char c)
+void terminal_increment(void)
 {
-  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-  if (++terminal_column == VGA_WIDTH)
-  {
     terminal_column = 0;
     if (++terminal_row == VGA_HEIGHT)
       terminal_row = 0;
+}
+
+void terminal_putchar(char c)
+{
+  if (c == '\n') terminal_increment();
+  else
+  {
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH) terminal_increment();
   }
 }
 
@@ -125,4 +131,5 @@ void kernel_main(void)
 
   /* newline character is not supported yet... */
   terminal_writestring("Hello, mOrT3m :)\n");
+  terminal_writestring("Qui docet, discit\n");
 }
